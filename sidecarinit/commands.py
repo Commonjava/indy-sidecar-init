affb6b3b6ca70668237056885148ee084a0ddf87
liyu <liyu@redhat.com>
2021-08-24 11:17:27 +0800

    add resource limit to template

--- sidecarinit/commands.py
+++ sidecarinit/commands.py
@@ -1,11 +1,12 @@
 import click
 import os
+import sys
 from pathlib import Path
 import sidecarinit.config as config
 import sidecarinit.download as download
 
 @click.command()
-@click.argument('env_yml', required=False, default='/deployments/config/application.yml') #help='Target environment, Same as sidecar application.yml'
+@click.argument('env_yml', required=False, default='/deployments/config/application.yaml') #help='Target environment, Same as sidecar application.yml'
 def run(env_yml):
     
     suite = config.read_config(env_yml)
@@ -17,3 +18,5 @@ def run(env_yml):
     Path(suite.local_repository + archive_name ).touch(exist_ok=True)
 
     download.download_archive(suite.archive_api + archive_name, suite.local_repository)
+
+    sys.exit(0);
