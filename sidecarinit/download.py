16853bb95bb734d110cdf575fa0f8187c6537862
yma <yma@redhat.com>
2023-03-28 16:50:16 +0800

    Add more console outputs for archive download and unpack

--- sidecarinit/download.py
+++ sidecarinit/download.py
@@ -8,16 +8,20 @@ def download_archive(url, repository):
     archive_path = f"/{build_id}.zip"
 
     try:
+      print(f"Trying to download archive from url {url}\n")
       with urllib.request.urlopen(url) as response:
           with open(repository + archive_path, 'wb') as out_file:
               out_file.write(response.read())
       downloaded = True
     except urllib.error.HTTPError as e:
       downloaded = False
-      if e.code not in [404]:
-        # log error
+      if e.code == 404:
+        print("Archive not found\n")
+      else:
         print(f"{e.code}: {e.reason}\n\n{e.headers}")
 
     if downloaded:
+      print(f"Archive successfully downloaded. Unpacking it in {repository}\n")
       with ZipFile(repository + archive_path) as archive_zip:
           archive_zip.extractall(repository)
+          print("Unpacked successfully\n")
