55c5bc38a6207240040fc419490d6cd8b4505ee4
yma <yma@redhat.com>
2023-03-28 13:25:53 +0800

    Fix error response handling for archive retrieving

--- sidecarinit/download.py
+++ sidecarinit/download.py
@@ -7,9 +7,17 @@ def download_archive(url, repository):
     build_id = os.environ.get('BUILD_CONFIG_ID')
     archive_path = f"/{build_id}.zip"
 
-    with urllib.request.urlopen(url) as response:
-        with open(repository + archive_path, 'wb') as out_file:
-            out_file.write(response.read())
+    try:
+      with urllib.request.urlopen(url) as response:
+          with open(repository + archive_path, 'wb') as out_file:
+              out_file.write(response.read())
+      downloaded = True
+    except urllib.error.HTTPError as e:
+      downloaded = False
+      if e.code not in [404]:
+        # log error
+        print(f"{e.code}: {e.reason}\n\n{e.headers}")
 
-    with ZipFile(repository + archive_path) as archive_zip:
-        archive_zip.extractall(repository)
+    if downloaded:
+      with ZipFile(repository + archive_path) as archive_zip:
+          archive_zip.extractall(repository)
